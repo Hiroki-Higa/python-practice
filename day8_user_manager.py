4b218b00d6e9a43e055b57b6568bf765209653a8
Hiroki-Higa <baltan444no4@gmail.com>
2025-12-30 12:42:39 +0900

    Day8: refactor user app using class

---
+++ day8_user_manager.py
@@ -0,0 +1,42 @@
+class UserManager:
+    def __init__(self):
+        self.users = []
+    
+    def add_user(self, name, age):
+        self.users.append({"name": name, "age": age})
+
+    def find_user(self, name):
+        for user in self.users:
+            if user["name"] == name:
+                return user
+        return None
+    
+def show_menu():
+        print("1: ユーザー追加")
+        print("2: ユーザー検索")
+        print("3: 終了")
+
+def main():
+    manager = UserManager()
+
+    while True:
+        show_menu()
+        choice = input("選択してください: ")
+
+        if choice == "1":
+            name = input("名前: ")
+            age = int(input("年齢: "))
+            manager.add_user(name, age)
+        
+        elif choice == "2":
+            name = input("検索する名前: ")
+            user = manager.find_user(name)
+            if user:
+                print(user)
+            else:
+                print("見つかりません")
+        
+        elif choice == "3":
+            break
+
+main()
